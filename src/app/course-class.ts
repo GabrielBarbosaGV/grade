c8964b152f27ae0a2574cc2a5de67a528f98a2a5
GabrielBarbosaGV <gbgv@cin.ufpe.br>
2019-01-31 15:14:54 +0000

    CourseClass's auxiliaries's constructors and Schedule's TimeLocation insertion methods.

--- src/app/course-class.ts
+++ src/app/course-class.ts
@@ -7,15 +7,66 @@ export default class CourseClass {
 
 export class Schedule {
     timeLocations: TimeLocation[];
+    noIntersect: boolean;
+
+    constructor(timeLocations: TimeLocation[], noIntersect: boolean = true) {
+        this.timeLocations = timeLocations;
+        this.noIntersect = noIntersect;
+
+        if (this.noIntersect) {
+            if (this.hasInternalIntersection(timeLocations)) {
+                let errMessage: string = "There are intersecting TimeLocations in given set, and noIntersect is set to true.";
+                throw new Error(errMessage);
+            }
+        }
+    }
+
+    insert(...timeLocations: TimeLocation[]) {
+        if (this.noIntersect
+                && (this.haveIntersection(this.timeLocations, timeLocations)
+                || this.hasInternalIntersection(timeLocations))) {
+            let errMessage = "Attempted to insert intersecting TimeLocation(s), but noIntersect is set to true.";
+            throw new Error(errMessage);
+        } else {
+            this.timeLocations = [...this.timeLocations, ...timeLocations];
+        }
+    }
+
+    insertArray(timeLocations: TimeLocation[]) {
+        this.insert(...timeLocations);
+    }
+
+    insertSchedule(schedule: Schedule) {
+        this.insertArray(schedule.timeLocations);
+    }
 
     intersectsWith(schedule: Schedule) {
-        for (let tl0 of this.timeLocations) {
-            for (let tl1 of schedule.timeLocations) {
-                if (tl0.intersectsWith(tl1)) return true;
-            }
+        return this.haveIntersection(this.timeLocations, schedule.timeLocations);
+    }
+
+    hasIntersection(timeLocation: TimeLocation, timeLocations: TimeLocation[]) {
+        for (let tl of timeLocations) {
+            if (tl.intersectsWith(timeLocation)) return true;
         }
 
         return false;
+    }
+
+    haveIntersection(timeLocations0: TimeLocation[], timeLocations1: TimeLocation[]) {
+        for (let tl0 of timeLocations0) {
+            if (this.hasIntersection(tl0, timeLocations1)) return true;
+        }
+
+        return false;
+    }
+
+    hasInternalIntersection(timeLocations: TimeLocation[]) {
+        for (let tl0 of timeLocations) {
+            for (let tl1 of timeLocations) {
+                if ((tl0 !== tl1) && tl0.intersectsWith(tl1))
+                    return true;
+            }
+        }
     }
 }
 
@@ -23,6 +74,12 @@ export class TimeLocation {
     day: number;
     startingTime: TimeStamp;
     endingTime: TimeStamp;
+
+    constructor(day: number, startingTime: TimeStamp, endingTime: TimeStamp) {
+        this.day = day;
+        this.startingTime = startingTime;
+        this.endingTime = endingTime;
+    }
 
     intersectsWith(timeLocation: TimeLocation) {
         if (timeLocation.startingTime.isEarlierThan(this.startingTime)) {
@@ -36,6 +93,11 @@ export class TimeLocation {
 export class TimeStamp {
     hour: number;
     minute: number;
+
+    constructor(hour: number, minute: number) {
+        this.hour = hour;
+        this.minute = minute;
+    }
 
     isEarlierThan(timeStamp: TimeStamp) {
         if (this.hour < timeStamp.hour) return true;
