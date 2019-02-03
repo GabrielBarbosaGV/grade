12a833a2c6e35af1c149cfd1d74388cff2d1a1bc
GabrielBarbosaGV <gbgv@cin.ufpe.br>
2019-02-03 16:47:05 +0000

    Tweaking timebox component.

--- src/app/timebox/timebox.component.ts
+++ src/app/timebox/timebox.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TimeLocation, TimeStamp } from '../course-class'
 
 @Component({
   selector: 'app-timebox',
@@ -10,11 +11,31 @@ export class TimeboxComponent implements OnInit {
   @Input() name: string;
   @Input() teacher: string;
   @Input() backgroundColor: string;
+  @Input() timeLocation: TimeLocation;
+
+  style = {
+    "background-color": "rgb(0, 0, 0)",
+    "position": "absolute",
+    "top": "0%",
+    "height": "0%",
+    "width": "100%",
+    "color": "rgb(255, 255, 255)"
+  }
   
 
   constructor() { }
 
   ngOnInit() {
+    //the top attribute will be how far the staringTime TimeStamp is from midnight
+    //we want this translated to a percentage
+    let distMidnight = this.timeLocation.startingTime.diffInSeconds(new TimeStamp(0));
+    this.style["top"] = ((distMidnight / (24 * 60 * 60)) * 100).toString() + '%';
+
+    //height will be the distance from the TimeLocation's first TimeStamp to the second
+    let innerDist = this.timeLocation.endingTime.diffInSeconds(this.timeLocation.startingTime);
+    this.style["height"] = ((innerDist / (24 * 60 * 60)) * 100).toString() + '%';
+
+    this.style["background-color"] = this.backgroundColor;
   }
 
 }
