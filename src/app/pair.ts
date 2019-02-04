33c698e3fe28c320c55b097915ae97f3b9cb5183
GabrielBarbosaGV <gbgv@cin.ufpe.br>
2019-02-04 20:52:07 +0000

    Created pair class.

---
+++ src/app/pair.ts
@@ -0,0 +1,9 @@
+export default class Pair<T1, T2> {
+    first: T1;
+    second: T2;
+
+    constructor(first: T1, second: T2) {
+        this.first = first;
+        this.second = second;
+    }
+}
